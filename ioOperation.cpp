a4e41c16b4ed0f0b8684ab41dbf01de7521cda34
Nishant Baheti <nishantbaheti.it19@gmail.com>
2020-10-01 19:21:07 +0530

    updates

---
+++ ioOperation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cout << "First digit :";
+    cin >> a;
+
+    cout << "Second digit ";
+    cin >> b;
+
+    cout << "Sum :" << a + b;
+
+    return 0;
+}
---
+++ operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 2;
+
+    cout << "a ++ " << a++;
+    cout << "++ a " << ++a;
+    return 0;
+}
